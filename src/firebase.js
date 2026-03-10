ee6734c0c767870e8e5b6f06e6e5f72244907e0a
Oldu8 <dudkoleg@gmail.com>
2026-03-10 20:27:55 +0100

    add firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgk34hN94kWCYUdnb4kPpnXrLadMyrToQ",
+  authDomain: "quiz-push-app.firebaseapp.com",
+  projectId: "quiz-push-app",
+  storageBucket: "quiz-push-app.firebasestorage.app",
+  messagingSenderId: "78267570013",
+  appId: "1:78267570013:web:cfadf475f6b6c2150a3ab4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
